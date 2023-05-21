c19fcf2cc5cb870e3480b411cf18d70788832409
Maria <mariaed.cassiano@gmail.com>
2023-05-21 19:33:52 -0300

    Resolvendo as injeções de dependências

--- API_DEVIO_COMPLETA/Configuration/DependencyInjectionConfig.cs
+++ API_DEVIO_COMPLETA/Configuration/DependencyInjectionConfig.cs
@@ -1,6 +1,8 @@
 ﻿using DevIO.Business.Intefaces;
 using Data.Data.Context;
 using Data.Data.Repository;
+using DevIO.Business.Notificacoes;
+using DevIO.Business.Services;
 
 namespace WEBAPI.Configuration
 {
@@ -9,7 +11,13 @@ namespace WEBAPI.Configuration
         public static IServiceCollection ResolveDependencies(this IServiceCollection services)
         {
             services.AddScoped<MeuDbContext>();
+            services.AddScoped<IProdutoRepository, ProdutoRepository>();
             services.AddScoped<IFornecedorRepository, FornecedorRepository>();
+            services.AddScoped<IEnderecoRepository, EnderecoRepository>();
+
+            services.AddScoped<INotificador, Notificador>();
+            services.AddScoped<IFornecedorService, IFornecedorService>();
+            services.AddScoped<IProdutoService, ProdutoService>();
 
             return services;
         }
