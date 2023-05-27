2a399e2c6e3315dd5f8e32520c95f26382e2d734
Maria <mariaed.cassiano@gmail.com>
2023-05-27 11:47:49 -0300

    Criado arquivo para fazer o bind de Json e Form data

---
+++ API_DEVIO_COMPLETA/Extensions/ProdutoModelBinder.cs
@@ -0,0 +1,31 @@
+﻿using API_DEVIO_COMPLETA.DTOs;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Text.Json;
+
+namespace API_DEVIO_COMPLETA.Extensions
+{
+    // Binder personalizado para envio de IFormFile e ViewModel dentro de um FormData compatível com .NET Core 3.1 ou superior (system.text.json)
+    public class ProdutoModelBinder : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            if (bindingContext == null)
+            {
+                throw new ArgumentNullException(nameof(bindingContext));
+            }
+
+            var serializeOptions = new JsonSerializerOptions
+            {
+                WriteIndented = true,
+                Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+                PropertyNameCaseInsensitive = true
+            };
+
+            var produtoImagemDTO = JsonSerializer.Deserialize<ProdutoImagemDTO>(bindingContext.ValueProvider.GetValue("produto").FirstOrDefault(), serializeOptions);
+            produtoImagemDTO.ImagemUpload = bindingContext.ActionContext.HttpContext.Request.Form.Files.FirstOrDefault();
+
+            bindingContext.Result = ModelBindingResult.Success(produtoImagemDTO);
+            return Task.CompletedTask;
+        }
+    }
+}
