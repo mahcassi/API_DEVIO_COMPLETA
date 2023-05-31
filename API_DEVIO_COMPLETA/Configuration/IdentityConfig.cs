2eae31bd2626c5a8e968f82be729bbfee18e6356
Maria <mariaed.cassiano@gmail.com>
2023-05-30 21:37:11 -0300

    Configurando o Context Identity

---
+++ API_DEVIO_COMPLETA/Configuration/IdentityConfig.cs
@@ -0,0 +1,19 @@
+﻿
+using API_DEVIO_COMPLETA.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_DEVIO_COMPLETA.Configuration
+{
+    public static class IdentityConfig
+    {
+        public static IServiceCollection AddIdentityConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            return services;
+        }
+    }
+}
