7890b97190a1239a9aea5d0dfd4076957b6bff74
Maria <mariaed.cassiano@gmail.com>
2023-05-30 22:07:16 -0300

    Criada DTO de user

---
+++ API_DEVIO_COMPLETA/DTOs/UserDTO.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API_DEVIO_COMPLETA.DTOs
+{
+    public class RegisterUserDTO
+    {
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [EmailAddress(ErrorMessage = "O campo {0} está em formato inválido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 6)]
+        public string Passwoord { get; set; }
+
+        [Compare("Password", ErrorMessage = "As senhas não conferem")]
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class LoginUserDTO
+    {
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [EmailAddress(ErrorMessage = "O campo {0} está em formato inválido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 6)]
+        public string Passwoord { get; set; }
+    }
+}
