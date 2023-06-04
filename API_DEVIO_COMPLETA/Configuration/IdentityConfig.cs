960660372ae3af6460ea5f749bf24d09012fc807
Maria <mariaed.cassiano@gmail.com>
2023-06-04 19:58:55 -0300

    Configurando o Json Web Token dentro do Identity

--- API_DEVIO_COMPLETA/Configuration/IdentityConfig.cs
+++ API_DEVIO_COMPLETA/Configuration/IdentityConfig.cs
@@ -1,8 +1,12 @@
 ﻿
 using API_DEVIO_COMPLETA.Data;
+using API_DEVIO_COMPLETA.Extensions;
 using DevIO.Api.Extensions;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace API_DEVIO_COMPLETA.Configuration
 {
@@ -20,6 +24,34 @@ namespace API_DEVIO_COMPLETA.Configuration
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddErrorDescriber<IdentityMensagensPortugues>()
                 .AddDefaultTokenProviders();
+
+            //JWT
+            var appSettingsSection = configuration.GetSection("AppSettings");
+            services.Configure<AppSettings> (appSettingsSection);
+
+            var appSettings = appSettingsSection.Get<AppSettings>();
+            var key = Encoding.ASCII.GetBytes(appSettings.Secret); // pegando o secret e fazer o encoding do secret
+
+
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = true;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters 
+                { 
+                    ValidateIssuerSigningKey = true, // valida se quem esta emitindo é o mesmo que vc recebeu no token
+                    IssuerSigningKey = new SymmetricSecurityKey(key), // configurando a chave
+                    ValidateIssuer = true, // valida o issuer conforme o nome do emissor
+                    ValidateAudience = true, // valida onde o token é valido
+                    ValidAudience = appSettings.ValidoEm, // apontando que é o audience
+                    ValidIssuer = appSettings.Emissor // apontando quem é o emissor
+                };
+
+            });
 
             return services;
         }
---
+++ API_DEVIO_COMPLETA/Extensions/AppSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace API_DEVIO_COMPLETA.Extensions
+{
+    public class AppSettings
+    {
+        public string Secret { get; set; }
+        public int ExpiracaoHoras { get; set; }
+        public string Emissor { get; set; }
+        public string ValidoEm { get; set; }
+    }
+}
