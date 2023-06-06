b968c893623c9612ef0a434772eed9d005890af1
Maria <mariaed.cassiano@gmail.com>
2023-06-05 21:50:53 -0300

    Criada classes de customização das Claims

---
+++ API_DEVIO_COMPLETA/Extensions/CustomAuthorize.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Security.Claims;
+
+namespace API_DEVIO_COMPLETA.Extensions
+{
+    public class CustomAuthorization
+    {
+        public static bool ValidarClaimsUsuario(HttpContext context, string claimName, string claimValue)
+        {
+            return context.User.Identity.IsAuthenticated &&
+                   context.User.Claims.Any(c => c.Type == claimName && c.Value.Contains(claimValue));
+        }
+    }
+
+    //Usada para aplicar autorização baseada em claims em um controlador ou ação específica.
+
+    //o Arguments é um mecanismo para fornecer dados ao filtro RequisitoClaimFilter por meio do sistema de injeção de dependência, permitindo a passagem do objeto
+    // Claim contendo o nome e valor das reivindicações para a classe RequisitoClaimFilter
+    public class ClaimsAuthorizeAttribute : TypeFilterAttribute
+    {
+        public ClaimsAuthorizeAttribute(string claimName, string claimValue) : base(typeof(RequisitoClaimFilter))
+        {
+            Arguments = new object[] { new Claim(claimName, claimValue) };
+        }
+    }
+
+    // Filtro de autorização
+    public class RequisitoClaimFilter : IAuthorizationFilter
+    {
+        private readonly Claim _claim;
+
+        public RequisitoClaimFilter(Claim claim)
+        {
+            _claim = claim;
+        }
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            if (!context.HttpContext.User.Identity.IsAuthenticated)
+            {
+                context.Result = new StatusCodeResult(401);
+                return;
+            }
+
+            if (!CustomAuthorization.ValidarClaimsUsuario(context.HttpContext, _claim.Type, _claim.Value))
+            {
+                context.Result = new StatusCodeResult(403);
+            }
+        }
+    }
+}
