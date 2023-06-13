b6e62d67eb6ee96068aa5a8717185809d9160289
Maria <mariaed.cassiano@gmail.com>
2023-06-12 21:36:35 -0300

    Criada controllers de teste para validar o versionamento

---
+++ API_DEVIO_COMPLETA/V1/Controllers/TesteController.cs
@@ -0,0 +1,21 @@
+﻿using API_DEVIO_COMPLETA.Controllers;
+using DevIO.Business.Intefaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_DEVIO_COMPLETA.V1.Controllers
+{
+    [ApiVersion("1.0", Deprecated = true)]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class TesteController : MainController
+    {
+        public TesteController(INotificador notificador, IUser appUser) : base(notificador, appUser)
+        {
+        }
+
+        [HttpGet]
+        public string Valor()
+        {
+            return "Sou a V1";
+        }
+    }
+}
---
+++ API_DEVIO_COMPLETA/V2/Controllers/TesteController.cs
@@ -0,0 +1,21 @@
+﻿using API_DEVIO_COMPLETA.Controllers;
+using DevIO.Business.Intefaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_DEVIO_COMPLETA.V2.Controllers
+{
+    [ApiVersion("2.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class TesteController : MainController
+    {
+        public TesteController(INotificador notificador, IUser appUser) : base(notificador, appUser)
+        {
+        }
+
+        [HttpGet]
+        public string Valor()
+        {
+            return "Sou a V2";
+        }
+    }
+}
