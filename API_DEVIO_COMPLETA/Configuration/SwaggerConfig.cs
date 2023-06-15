e37043eb604e77a37f8136cac5100248732c7553
Maria <mariaed.cassiano@gmail.com>
2023-06-14 21:45:05 -0300

    Documentando a API com Swagger

---
+++ API_DEVIO_COMPLETA/Configuration/SwaggerConfig.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Text.Json;
+
+namespace API_DEVIO_COMPLETA.Configuration
+{
+
+    public static class SwaggerConfig
+    {
+        public static IServiceCollection AddSwaggerConfig(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.OperationFilter<SwaggerDefaultValues>();
+            });
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseSwaggerConfig(this IApplicationBuilder app, IApiVersionDescriptionProvider provider)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(op =>
+            {
+                foreach (var description in provider.ApiVersionDescriptions)
+                {
+                    op.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json", description.GroupName.ToUpperInvariant());
+                }
+            });
+
+            return app;
+        }
+    }
+
+    //Extendendo a opção de configuração do Swagger
+    public class ConfigureSwaggerOptions : IConfigureOptions<SwaggerGenOptions>
+    {
+        readonly IApiVersionDescriptionProvider provider;
+
+        public ConfigureSwaggerOptions(IApiVersionDescriptionProvider provider)
+        {
+            this.provider = provider;
+        }
+
+
+        //pegando todas as versões da minha api e adicionando um doc para cada uma delas
+        public void Configure(SwaggerGenOptions options)
+        {
+            foreach (var description in provider.ApiVersionDescriptions)
+            {
+                options.SwaggerDoc(description.GroupName, CreateInfoForApiVersion(description));
+            }
+        }
+
+        // Criando documentação mínima da minha API
+        static OpenApiInfo CreateInfoForApiVersion(ApiVersionDescription description)
+        {
+            var info = new OpenApiInfo()
+            {
+                Title = "API - desenvolvedor.io",
+                Version = description.ApiVersion.ToString(),
+                Description = "Esta API faz parte do curso REST com ASP.NET Core WebAPI.",
+                Contact = new OpenApiContact() { Name = "Maria Eduarda Cassiano da Silva", Email = "mariaed.cassiano@gmail.com" },
+                License = new OpenApiLicense() { Name = "MIT", Url = new Uri("https://opensource.org/licenses/MIT") }
+            };
+
+            if (description.IsDeprecated)
+            {
+                info.Description += " Esta versão está obsoleta!";
+            }
+
+            return info;
+        }
+
+    }
+
+    public class SwaggerDefaultValues : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            var apiDescription = context.ApiDescription;
+
+            operation.Deprecated |= apiDescription.IsDeprecated();
+
+            if (operation.Parameters == null)
+                return;
+
+            foreach (var parameter in operation.Parameters)
+            {
+                var description = apiDescription.ParameterDescriptions.First(p => p.Name == parameter.Name);
+
+                parameter.Description ??= description.ModelMetadata.Description;
+
+                if (parameter.Schema.Default == null && description.DefaultValue != null)
+                {
+                    var json = JsonSerializer.Serialize(description.DefaultValue, description.ModelMetadata.ModelType);
+                    parameter.Schema.Default = OpenApiAnyFactory.CreateFromJson(json);
+                }
+
+                parameter.Required |= description.IsRequired;
+            }
+        }
+    }
+}
